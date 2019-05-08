bee2ff0b088081a1b2c58039ce1b2adb5e70242f
Максим <defoltmaks@gmail.com>
2019-05-08 22:48:23 +0300

    add new block

--- customer/scrypt.js
+++ customer/scrypt.js
@@ -25,18 +25,54 @@ function getPrice() {
 $('.custom-select').change(function (){
     getPrice();
 });
+ 
+if(window.innerWidth <= 767){
+    $('.order-2').addClass('order-0');
+    $('.order-2').removeClass('order-2');
+}
+
+window.onload = function () {
+
+    var after = {
+        animationEnabled: true,
+        title: {
+            text: "До"
+        },
+        data: [{
+            type: "doughnut",
+            innerRadius: "40%",
+            indexLabel: "{label}: #percent%",
+            dataPoints: [
+                { label: "Vk", y: 20 },
+                { label: "Сайт", y: 15 },
+                { label: "Телефонный звонок", y: 15 },
+                { label: "Мессенджеры", y: 5 },
+                { label: "Уход", y: 45 }
+            ]
+        }]
+    };
+    var before = {
+        animationEnabled: true,
+        title: {
+            text: "После"
+        },
+        data: [{
+            type: "doughnut",
+            innerRadius: "40%",
+            indexLabel: "{label}: #percent%",
+            dataPoints: [
+                { label: "Vk", y: 35 },
+                { label: "Сайт", y: 30 },
+                { label: "Телефонный звонок", y: 20 },
+                { label: "Мессенджеры", y: 10 },
+                { label: "Уход", y: 5 }
+            ]
+        }]
+    };
+    $("#chartContainer").CanvasJSChart(after);
+    $("#chartContainer1").CanvasJSChart(before);
+    
+    }
 
 
-
-
-// $('.custom-select').click(function () { 
-//     summ -= getValue();
-//     $('#price').val(summ);
-//     $('.custom-select').change(function () { 
-//         summ += getValue();
-//         $('#price').val(summ);
-//     });
-// });
-
-
-// 2.0
+    
